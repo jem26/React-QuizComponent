3eb149cc1c7e1f7e113ca5f772fef04d8c21db40
Jamie Maher <jmaher5@nd.edu>
2018-09-05 13:27:14 -0500

    5

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,6 +5,11 @@ import QuizEnd from './QuizEnd.js'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
+  showNextQuestion() {
+    this.setState((state) => {
+      return {quiz_position: state.quiz_position + 1}
+    })
+  }
   constructor(props) {
     super(props)
     this.state = { quiz_position: 1 }
@@ -15,7 +20,8 @@ class Quiz extends Component {
       <div>
         {isQuizEnd ?
         <QuizEnd/> :
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+        showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
       </div>
     )
   }
